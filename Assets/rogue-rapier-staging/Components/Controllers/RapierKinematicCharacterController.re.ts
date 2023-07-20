6e999088108c2748ba2d81624919f9926f27e7a7
Justin Gilman <eyeofmidas@gmail.com>
2023-07-20 08:26:38 -0700

    fixing scene items; cleaning up controller code

--- Assets/rogue-rapier-staging/Components/Controllers/RapierKinematicCharacterController.re.ts
+++ Assets/rogue-rapier-staging/Components/Controllers/RapierKinematicCharacterController.re.ts
@@ -4,6 +4,7 @@ import * as THREE from 'three'
 import RapierCollider from '@RE/RogueEngine/rogue-rapier/Components/Colliders/RapierCollider'
 import RogueRapier from '@RE/RogueEngine/rogue-rapier/Lib/RogueRapier'
 import RapierBody from '@RE/RogueEngine/rogue-rapier/Components/RapierBody.re'
+import RapierConfig from '@RE/RogueEngine/rogue-rapier/Components/RapierConfig.re'
 
 export default class RapierKinematicCharacterController extends RE.Component {
   @RE.props.num() offset = 0.1
@@ -15,21 +16,23 @@ export default class RapierKinematicCharacterController extends RE.Component {
   @RE.props.checkbox() applyImpulsesToDynamicBodies = true
   @RE.props.checkbox() slideEnabled = true
   @RE.props.num() jumpHeight = 10
+  @RE.props.select() type = 0;
+  typeOptions = ["KinematicPositionBased", "KinematicVelocityBased"];
 
-
+  
   initialized = false
   characterController: RAPIER.KinematicCharacterController
 
   @RE.props.num() speed = 0.1
-  characterCollider: RAPIER.Collider | undefined
+  characterColliders: RapierCollider[] = []
   movementDirection = new THREE.Vector3(0.0, 0.0, 0.0)
-  rigidBody: RAPIER.RigidBody
-
-  @RE.props.select() type = 0;
-  typeOptions = ["KinematicPositionBased", "KinematicVelocityBased"];
+  rapierBodyComponent: RapierBody
+  rapierConfig: RapierConfig
 
   //https://github.com/dimforge/rapier.js/blob/master/testbed3d/src/demos/characterController.ts
   awake() {
+    this.rapierConfig = RE.getComponent(RapierConfig) as RapierConfig
+    this.rapierBodyComponent = RE.getComponent(RapierBody, this.object3d) as RapierBody  
   }
 
   start() {
@@ -39,37 +42,6 @@ export default class RapierKinematicCharacterController extends RE.Component {
   beforeUpdate(): void {
     if (!RogueRapier.initialized) return;
     !this.initialized && this.init();
-
-    // this.type !== RAPIER.RigidBodyType.Fixed && 
-    // this.updatePhysics();
-    const component = RE.getComponent(RapierBody, this.object3d);
-    if (!component) {
-      RE.Debug.logError("did not find body")
-    } else {
-      this.rigidBody = component.body
-
-
-      const colliderComponent = this.getColliderComponentFromAChild(this.object3d)
-      if (colliderComponent) {
-        this.characterCollider = colliderComponent[0].collider
-      }
-
-    }
-  }
-
-  getColliderComponentFromAChild(object3d) {
-    const rapierColliders: RapierCollider[] = [];
-
-    object3d.traverse(obj => {
-      const components = RE.getObjectComponents(obj);
-
-      components.forEach(comp => {
-        if (comp instanceof RapierCollider) {
-          rapierColliders.push(comp);
-        }
-      })
-    })
-    return rapierColliders
   }
 
   init() {
@@ -82,50 +54,63 @@ export default class RapierKinematicCharacterController extends RE.Component {
   }
 
   update() {
-    
-    if (!this.rigidBody) {
+    if (!this.rapierBodyComponent.body) {
       RE.Debug.logWarning("No character body")
       return
     }
 
-    if (!this.characterCollider) {
+    if (this.rapierBodyComponent.body.numColliders() < 1) {
       RE.Debug.logWarning("No character collider")
       return
     }
 
-    const gravity = -9.81
+
+    switch (this.type) {
+      case 0:
+        this.handleKinematicPositionBased()
+        break;
+      case 1:
+        this.handleKinematicVelocityBased()
+        break;
+    }
+  }
+
+  handleKinematicPositionBased() {
+    
+    const gravity = this.rapierConfig.gravity
+    const fixedStep = RE.Runtime.deltaTime
+
     const playerVelocity = new THREE.Vector3(0, 0, 0)
 
     const scaledMovement = new THREE.Vector3(this.movementDirection.x, 0, this.movementDirection.z)
     scaledMovement.normalize()
-    // RE.Debug.log(`Moving ${JSON.stringify(this.movementDirection)}`)
-    
+
     playerVelocity.x = scaledMovement.x * this.speed
     playerVelocity.z = scaledMovement.z * this.speed
-    const nextPosition = this.rigidBody.translation()
+    const nextPosition = this.rapierBodyComponent.body.translation()
     const isGrounded = this.characterController.computedGrounded()
-   if(isGrounded) {
-    RE.Debug.log(`grounded`)
-   }
-    if(isGrounded && playerVelocity.y != 0) {
+
+    if (isGrounded) {
+      RE.Debug.log(`grounded`)
+    }
+
+    if (isGrounded && playerVelocity.y != 0) {
       playerVelocity.y = 0
     }
 
-    let fixedStep = RE.Runtime.deltaTime
-    // RE.Debug.log(`${RE.Runtime.clock.getDelta()}`)
     if (isGrounded && this.movementDirection.y != 0) {
       RE.Debug.log("jumping")
       playerVelocity.y += Math.sqrt(
-        this.jumpHeight * 3 * (gravity * (fixedStep)),
+        this.jumpHeight * 3 * (gravity.y * fixedStep),
       )
     }
 
-    playerVelocity.y += gravity * (fixedStep)
-
-    // RE.Debug.log(`Moving ${JSON.stringify(playerVelocity)}`)
+    playerVelocity.x += gravity.x * fixedStep
+    playerVelocity.y += gravity.y * fixedStep
+    playerVelocity.z += gravity.z * fixedStep
 
     this.characterController.computeColliderMovement(
-      this.rigidBody.collider(0),
+      this.rapierBodyComponent.body.collider(0),
       playerVelocity,
     )
 
@@ -135,24 +120,11 @@ export default class RapierKinematicCharacterController extends RE.Component {
     nextPosition.y += characterMovement.y
     nextPosition.z += characterMovement.z
 
-    this.rigidBody.setNextKinematicTranslation(nextPosition)
+    this.rapierBodyComponent.body.setNextKinematicTranslation(nextPosition)
+  }
 
-    
-
-    // switch(this.type) {
-    //   case 0:
-    //     // let movement = this.characterController.computedMovement()
-    //     // let newPos = this.character.translation()
-    //     // newPos.x += movement.x
-    //     // newPos.y += movement.y
-    //     // newPos.z += movement.z
-    //     // this.character.setNextKinematicTranslation(newPos)
-    //     break;
-    //   case 1:
-    //     let velocity = new RAPIER.Vector3(scaledMovementDirection.x, scaledMovementDirection.y, scaledMovementDirection.z)
-    //     this.character.setLinvel(velocity, true)
-    //     break;
-    // }
+  handleKinematicVelocityBased() {
+    RE.Debug.logError("Does not support Kinematic Velocity at this time.")
   }
 }
 
